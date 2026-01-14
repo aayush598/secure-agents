65620c2a213a328647a328ff7eaa6e41bd91b2ef
aayush598 <aayushgid598@gmail.com>
2026-01-14 11:39:05 +0530

    style: navbar component shared directory included in the tailwindcss config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,4 +7,17 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': ['error'],
     'no-console': ['error', { allow: ['warn', 'error'] }],
   },
+  overrides: [
+    {
+      files: [
+        'tailwind.config.js',
+        'postcss.config.js',
+        'next.config.js',
+        '*.config.js',
+      ],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'off',
+      },
+    },
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './components/**/*.{ts,tsx,js,jsx}',
     './app/**/*.{ts,tsx,js,jsx}',
     './src/**/*.{ts,tsx,js,jsx}',
+    './shared/**/*.{ts,tsx,js,jsx}',
   ],
   prefix: '',
   theme: {
